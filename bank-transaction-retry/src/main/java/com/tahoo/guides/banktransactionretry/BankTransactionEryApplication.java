8266815fa39cd9dc79b4f9f35876b939394dca54
Toni Aho <toni@tahoo.fi>
2024-06-02 11:02:16 +0300

    files

---
+++ bank-transaction-retry/src/main/java/com/tahoo/guides/banktransactionretry/BankTransactionEryApplication.java
@@ -0,0 +1,13 @@
+package com.tahoo.guides.banktransactionretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionEryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionEryApplication.class, args);
+	}
+
+}
