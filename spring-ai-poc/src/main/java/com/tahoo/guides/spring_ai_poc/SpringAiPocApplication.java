3f716fe2a759dd8f821faf8e79e2beb7fe6fb182
Toni Aho <toni@tahoo.fi>
2025-05-04 16:06:13 +0300

    tarvittavat

---
+++ spring-ai-poc/src/main/java/com/tahoo/guides/spring_ai_poc/SpringAiPocApplication.java
@@ -0,0 +1,13 @@
+package com.tahoo.guides.spring_ai_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPocApplication.class, args);
+	}
+
+}
---
+++ spring-ai-poc/src/test/java/com/tahoo/guides/spring_ai_poc/SpringAiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahoo.guides.spring_ai_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
