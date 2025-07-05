d3883c01c76216d28b2d6711bc6eb7ef667ddf4c
Toni Aho <toni@tahoo.fi>
2025-07-05 19:31:59 +0300

    korjauksia lisää

--- background-cache/src/main/java/com/tahoo/guides/background_cache/BackgroundCacheInitializationConfig.java
+++ background-cache/src/main/java/com/tahoo/guides/background_cache/BackgroundCacheInitializationConfig.java
@@ -2,6 +2,8 @@ package com.tahoo.guides.background_cache;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
 
 
 @Configuration
@@ -9,6 +11,8 @@ public class BackgroundCacheInitializationConfig {
 
 
   @Bean(bootstrap = Bean.Bootstrap.BACKGROUND)
+  @Lazy
+  @DependsOn("numbersGenerationService")
   CacheInitializer cacheInitializer(final NumbersGenerationService numbersGenerationService) {
     return new CacheInitializer(numbersGenerationService);
   }
