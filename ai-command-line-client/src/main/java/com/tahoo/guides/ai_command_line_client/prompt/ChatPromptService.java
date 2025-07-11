99227a743a190c4e6b394d583c89a76bdfe593cf
Toni Aho <toni@tahoo.fi>
2025-07-12 02:58:37 +0300

    korjaukset

--- ai-command-line-client/src/main/java/com/tahoo/guides/ai_command_line_client/prompt/ChatPromptService.java
+++ ai-command-line-client/src/main/java/com/tahoo/guides/ai_command_line_client/prompt/ChatPromptService.java
@@ -1,6 +1,8 @@
 package com.tahoo.guides.ai_command_line_client.prompt;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -10,11 +12,16 @@ public class ChatPromptService {
   private final ChatClient chatClient;
 
   public ChatPromptService(final ChatClient.Builder chatClientBuilder) {
-    this.chatClient = chatClientBuilder.build();
+    this.chatClient = chatClientBuilder
+            .defaultOptions(ChatOptions.builder()
+                    .model(OpenAiApi.ChatModel.GPT_4_1_MINI.getValue())
+                    .build())
+            .build();
   }
 
   public Flux<String> stream(final String question) {
     return chatClient.prompt()
+
             .user(question)
             .stream()
             .content();
